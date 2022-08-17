0674eea76acd59422e26c5fcc2c5473c34b3f289
Aleksandra Hrevtsova <47608805+AleksandraHrevtsova@users.noreply.github.com>
2022-08-18 01:11:34 +0300

    added selected components

--- src/i18n.js
+++ src/i18n.js
@@ -12,7 +12,14 @@ export function getLangFromLocalStorage(){
   return localStorage.getItem('lang')
 }
 
-export function getLang(){
+export function getInitLang(){
+  let lsLang = getLangFromLocalStorage();
+  lsLang = lsLang === 'ua' ? 'uk' : lsLang
+  // console.log('lsLang:', lsLang); // null
+  if(locales[lsLang]){
+    return lsLang
+  }
+
   const browserLang = getLangFromBrowser();
   // console.log('browserLang:', browserLang); 
   if(locales[browserLang]){
@@ -25,17 +32,10 @@ export function getLang(){
     return checkedLangFromBrowser
   }
 
-  let lsLang = getLangFromLocalStorage();
-  lsLang = lsLang === 'ua' ? 'uk' : lsLang
-  // console.log('lsLang:', lsLang); // null
-  if(locales[lsLang]){
-    return lsLang
-  }
-  
   return 'en'
 }
 
-const lang = getLang();
+export const lang = getInitLang();
 // console.log('LANG:', lang);
 
 function getLocalesByLang(lang) {
@@ -43,59 +43,8 @@ function getLocalesByLang(lang) {
 }
 const currentLocales = getLocalesByLang(lang);
 // console.log('result:', result);
+
 export function getLocale(key){
-  console.log('currentLocales[key]:', currentLocales[key]);
+  // console.log('currentLocales[key]:', currentLocales[key]);
   return currentLocales[key] || key
 }
-// console.log(getLocale('owr_work'));
-// console.log(getLocale('get_in_touch'));
-
-
-// import moment from 'moment';
-// import 'moment/locale/fi';
-// import * as locales from './locales';
-// const translations = { ...locales };
-// const availableLangs = ['en', 'fi'];
-
-// const getLanguageFromBrowser = () => {
-//   let langCode = (
-//     ('languages' in navigator ? navigator.languages[0] : null) ||
-//     navigator.language
-//   ).substring(0, 2);
-
-//   return langCode;
-// };
-
-// const getLang = () => {
-//   let currentLang = localStorage.getItem('lang');
-//   if (availableLangs.includes(currentLang)) {
-//     return currentLang;
-//   }
-//   currentLang = getLanguageFromBrowser();
-//   if (Object.prototype.hasOwnProperty.call(translations, currentLang)) {
-//     localStorage.setItem('lang', currentLang);
-//     return currentLang;
-//   }
-//   localStorage.setItem('lang', 'en');
-//   return 'en';
-// };
-
-// const getSelectableLanguages = () => {
-//   return Object.keys(translations);
-// };
-
-// export const setLanguage = (language) => {
-//   localStorage.setItem('lang', language);
-// };
-
-// export const langCode = getLang();
-
-// const localizations = translations[langCode];
-
-// const localize = (key) => {
-//   return localizations[key] || `${key}`;
-// };
-
-// export const selectableLanguages = getSelectableLanguages();
-
-// export default localize;
